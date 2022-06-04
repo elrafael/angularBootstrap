f40f7b72683e431631a0cc3ada543a614f4cd9e0
Rafael V. de Oliveira <elrafael123@gmail.com>
2022-06-04 19:49:24 +0100

    sidenav and dashboard component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopNavBarComponent } from './top-nav-bar/top-nav-bar.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { DashBoardComponent } from './dash-board/dash-board.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopNavBarComponent
+    TopNavBarComponent,
+    SideNavComponent,
+    DashBoardComponent
   ],
   imports: [
     BrowserModule,
